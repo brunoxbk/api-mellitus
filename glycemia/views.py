5dfd67cb02aab5615523c6df3dbe22316a7ad6bf
Wesley Bruno <brunoxbk@gmail.com>
2025-01-15 19:31:13 -0300

    ajuste filtro glicemia

--- glycemia/views.py
+++ glycemia/views.py
@@ -8,6 +8,9 @@ class GlycemiaLogListCreateView(generics.ListCreateAPIView):
     queryset = GlycemiaLog.objects.all()
     serializer_class = GlycemiaLogSerializer
 
+    def get_queryset(self):
+        return GlycemiaLog.objects.filter(user=self.request.user)
+
 
 class GlycemiaLogDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = GlycemiaLog.objects.all()
