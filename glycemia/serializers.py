dc1bb67a30efc8e5cb7de1b1b77479f1941aa057
bruno <brunoxbk@gmail.com>
2025-01-15 21:18:36 -0300

    error glicemia

--- glycemia/serializers.py
+++ glycemia/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from glycemia.models import GlycemiaLog
@@ -7,3 +8,19 @@ class GlycemiaLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = GlycemiaLog
         fields = "__all__"
+
+    def validate(self, attrs):
+
+        user = self.context.get("request").user
+
+        date_only = timezone.now().date()
+
+        if GlycemiaLog.objects.filter(
+            user=user, measurement_time__date=date_only
+        ).exists():
+            print("existe")
+            raise serializers.ValidationError(
+                "Já existe uma medição registrada para este usuário neste dia."
+            )
+
+        return super().validate(attrs)
